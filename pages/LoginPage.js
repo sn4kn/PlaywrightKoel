d18f44e936afa7b1cde4d3101e32a5b32f268c2e
sn4kn <x.sonechkin@gmail.com>
2025-06-13 21:36:54 -0500

    added login with valid credentials test

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,10 +1,14 @@
-export class LoginPage{
-    constructor(page){
-        this.page=page
-        this.logInButton= page.locator("[type='submit']")
+export class LoginPage {
+    constructor(page) {
+        this.page = page
+        this.logInButton = page.locator("[type='submit']")
         this.userName = page.locator("[type='email']")
         this.password = page.locator("[type='password']")
     }
 
-    
+    async validLogin(username, password) {
+        await this.userName.type(username);
+        await this.password.type(password);
+        await this.logInButton.click();
+    }
 }
--- tests/LoginPageTest.spec.js
+++ tests/LoginPageTest.spec.js
@@ -1,12 +1,14 @@
 import { test, expect } from '@playwright/test'
 import { LoginPage } from '../pages/LoginPage'
 
-test("login page title",async ({page})=>{
-await page.goto("/")
-await expect(page).toHaveTitle("Koel")
+test("login page title", async ({ page }) => {
+    await page.goto("/")
+    await expect(page).toHaveTitle("Koel")
 })
 
-test("login with valid credentials",async ({page})=>{
-await page.goto("/")
-await expect(page).toHaveTitle("Koel")
+test("login with valid credentials", async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    await page.goto("/")
+    await loginPage.validLogin("andrii.banak@testpro.io","YWmFWK7$QTp7M!9")
+    await expect(page).toHaveURL("https://qa.koel.app/#!/home")
 })
