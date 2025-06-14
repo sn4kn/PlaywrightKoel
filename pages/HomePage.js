15c68addc5605016a6bb1266bcf8ca9ee18d31d9
sn4kn <x.sonechkin@gmail.com>
2025-06-14 14:24:18 -0500

    added home page locators

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,5 +1,13 @@
 export class HomePage{
     constructor(page){
         this.page=page;
+        this.createPlaylistButton = page.locator("[title='Create a new playlist']")
+        this.newPlaylistButton = page.locator(".playlist-menu li:first-child")
+        this.createPlaylistField = page.locator("[placeholder='↵ to save']")
+        this.allSongs = page.locator("li .songs")
+        this.playlistName = page.locator("a:has-text('Rap')")
+        this.currentPlayingSongTitle = page.locator("#progressPane>h3")
+        this.deletePlaylistButton = page.locator([title="Delete this playlist"])
+        this.editPlaylistButton = page.locator("li:has-text('Edit')")
     }
 }
