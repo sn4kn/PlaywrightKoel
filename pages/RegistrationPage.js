ee644820f75062b78bc9600b5be87d1178e82bd6
sn4kn <x.sonechkin@gmail.com>
2025-06-15 12:51:53 -0500

    added should show error for personal email or invalid email format test

--- pages/RegistrationPage.js
+++ pages/RegistrationPage.js
@@ -2,8 +2,13 @@ export class RegistrationPage {
     constructor(page) {
         this.page = page
         this.logInButton = page.locator("[type='submit']")
-        
+        this.emailField = page.locator("[placeholder='Email']")
+        this.submitBtn = page.locator("[value='Submit']")
+        this.errMsg = page.locator("form div:has-text('Sorry, only certain emails are allowed, please do not use your personal email')")
     }
 
-    
+    async fillEmailAndSubmit(email) {
+        await this.emailField.type(email)
+        await this.submitBtn.click()
+    }
 }
--- tests/RegistrationTest.spec.js
+++ tests/RegistrationTest.spec.js
@@ -1,2 +1,9 @@
 import { test, expect } from '@playwright/test'
-import { LoginPage } from '../pages/LoginPage'
+import { RegistrationPage } from '../pages/RegistrationPage'
+
+test('should show error for personal email or invalid email format', async ({ page }) => {
+  const registrationPage = new RegistrationPage(page)
+  await page.goto('/registration')
+  await registrationPage.fillEmailAndSubmit("xxx@gmail.com")
+  await expect(registrationPage.errMsg).toBeVisible()
+});
