c287394d9c93f7b8cfd3002e955d133254ee0ffa
sn4kn <x.sonechkin@gmail.com>
2025-06-14 20:03:45 -0500

    added globalSetup.js and updated expect timeout in playwright.config.js file

--- playwright.config.js
+++ playwright.config.js
@@ -14,12 +14,17 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout : 30 * 1000,
+  expect : {
+    timeout : 5 * 1000
+  },
+  globalSetup : require.resolve("./globalSetup.js"),
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -28,28 +33,24 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://qa.koel.app/',
-
+    viewport: { width: 1920, height: 1080 },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1920, height: 1080 }, // override
+        launchOptions: {
+          args: ['--window-size=1920,1080'],
+        },
+      }
+    }
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-    
 
     /* Test against mobile viewports. */
     // {
@@ -72,7 +73,7 @@ export default defineConfig({
     // },
   ],
 
-  
+
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
--- tests/AllSongsTest.spec.js
+++ tests/AllSongsTest.spec.js
@@ -1,5 +1,3 @@
-import * as dotenv from "dotenv"
-dotenv.config()
 import { test, expect } from '@playwright/test'
 import { HomePage } from '../pages/HomePage'
 import { LoginPage } from '../pages/LoginPage'
